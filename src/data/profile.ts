eac7479616d13ecc1316a325fd0f6a81f9c102c3
Angela Rubalcava <angela@consilientlabs.com>
2026-05-18 19:23:19 -0700

    fixing headshot and resume details

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,7 +1,7 @@
 export const profile = {
   githubUrl: 'https://github.com/amrubal01',
   headshotAlt: 'Angela Rubalcava headshot',
-  headshotSrc: '/headshot.jpg',
+  headshotSrc: `${import.meta.env.BASE_URL}headshot.jpg`,
   linkedinUrl: 'https://www.linkedin.com/in/angela-rubalcava-9ab310217/',
-  resumeUrl: '/resume.pdf',
+  resumeUrl: `${import.meta.env.BASE_URL}resume.pdf`,
 };
